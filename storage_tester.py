a1f3f93d9c77dd6fbaebf20c402519013d393a53
Sijis Aviles <sijis.aviles+github@gmail.com>
2021-08-16 00:59:00 -0500

    style: format with black

--- storage_tester.py
+++ storage_tester.py
@@ -1,54 +1,55 @@
 from errbot import BotPlugin, botcmd
 
+
 class MyClass(object):
     pass
 
+
 class StorageTester(BotPlugin):
-    """ This tests the storage backend.
-    """
+    """This tests the storage backend."""
+
     @botcmd
     def teststorage(self, msg, args):
         for key in self:
-            self.log.debug('Cleaning up %s' % key)
+            self.log.debug("Cleaning up %s", key)
             del self[key]
 
-        self.log.debug('test length')
+        self.log.debug("test length")
         if len(self) != 0:
-            return 'ERROR: it should be empty'
+            return "ERROR: it should be empty"
 
-        self.log.debug('test set')
-        self['foo'] = 'bar'
-        if self['foo'] != 'bar':
-            return 'ERROR: did not store foo'
+        self.log.debug("test set")
+        self["foo"] = "bar"
+        if self["foo"] != "bar":
+            return "ERROR: did not store foo"
 
-        self.log.debug('test length 1')
+        self.log.debug("test length 1")
         if len(self) != 1:
-            return 'ERROR: Should be length 1'
+            return "ERROR: Should be length 1"
 
-        self.log.debug('test del')
-        del self['foo']
+        self.log.debug("test del")
+        del self["foo"]
 
         if len(self) != 0:
-            return 'ERROR: Should be length 0'
+            return "ERROR: Should be length 0"
 
+        self.log.debug("test class serialization")
+        self["clazz"] = MyClass()
 
-        self.log.debug('test class serialization')
-        self['clazz'] = MyClass()
-
-        self.log.debug('test class deserialization')
-        if type(self['clazz']) != MyClass:
-            return 'ERROR: Should be type MyClass'
+        self.log.debug("test class deserialization")
+        if type(self["clazz"]) != MyClass:
+            return "ERROR: Should be type MyClass"
 
         try:
-            self.log.debug('test missing key')
-            self.log.debug(self['unknown'])
-            return 'ERROR: should have send a KeyError'
+            self.log.debug("test missing key")
+            self.log.debug(self["unknown"])
+            return "ERROR: should have send a KeyError"
         except KeyError:
             pass
 
-        self.log.debug('test keys %s' % list(self.keys()))
-        if list(self.keys())[0] != 'clazz' and len(self.keys()) != 1:
-            return 'ERROR: key sets inconsistent'
+        self.log.debug("test keys %s", list(self.keys()))
+        if list(self.keys())[0] != "clazz" and len(self.keys()) != 1:
+            return "ERROR: key sets inconsistent"
 
-        self.log.debug('everything is OK')
-        return 'OK'
+        self.log.debug("everything is OK")
+        return "OK"
--- test_storage_tester.py
+++ test_storage_tester.py
@@ -1,5 +1,8 @@
 from errbot.backends.test import testbot
+
+
 class TestMyPlugin(object):
-    extra_plugin_dir = '.'
+    extra_plugin_dir = "."
+
     def test_command(self, testbot):
-        testbot.assertCommand('!teststorage', 'OK')
+        testbot.assertCommand("!teststorage", "OK")
